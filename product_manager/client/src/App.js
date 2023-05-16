a6ab9adcf4dd594f39fda905856d683b740cf225
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-16 18:38:31 -0500

    form on fronend

--- product_manager/client/src/App.js
+++ product_manager/client/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <fieldset class='container'>
+      <h1>Product Manager:</h1>
+      <form action="submit">
+        <label>Product:</label>
+        <input type="text" />
+        <label>Description:</label>
+        <input type="textArea" />
+        <label>Price:</label>
+        <input type="number" />
+        <button>Create Product</button>
+      </form>
+    </fieldset>
   );
 }
 

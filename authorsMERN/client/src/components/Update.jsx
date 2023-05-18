26fd64bbc796763fda34399718e7f4f4ede3f0c1
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-17 19:53:11 -0500

    getting update setup

--- authorsMERN/client/src/components/Update.jsx
+++ authorsMERN/client/src/components/Update.jsx
@@ -2,22 +2,29 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 
-
 const Update = (props) => {
+    const [authors, setAuthors] = useState([]);
 
     useEffect(() => {
-        axios.get('http://localhost:8000/api/authors')
-            .then(res => {
-                console.log(res.data.Authors);
+        axios
+            .get('http://localhost:8000/api/authors')
+            .then((res) => {
+                setAuthors(res.data.Authors);
             })
-            .catch(err => {
+            .catch((err) => {
                 console.log(err);
             });
     }, []);
+
     return (
+        <div>
+            {authors.map((author) => (
+                <fieldset key={author._id}>
+                    Update {author.author}
+                </fieldset>
+            ))}
+        </div>
+    );
+};
 
-        <fieldset>Update</fieldset>
-    )
-}
-
-export default Update
+export default Update;

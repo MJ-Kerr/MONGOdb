e66e0a6a2561837d9ee1115c75f855f624dac82c
Miles Kerr <m.kerr.professional@gmail.com>
2023-05-17 23:12:50 -0500

    authors is done finally

--- authorsMERN/client/src/components/Update.jsx
+++ authorsMERN/client/src/components/Update.jsx
@@ -1,30 +1,58 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate, Link } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { useParams, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
 const Update = (props) => {
+    const { id } = useParams()
     const [authors, setAuthors] = useState([]);
+    const [errors, setErrors] = useState([]);
+    const navigate = useNavigate();
 
     useEffect(() => {
-        axios
-            .get('http://localhost:8000/api/authors')
-            .then((res) => {
-                setAuthors(res.data.Authors);
+        axios.get('http://localhost:8000/api/authors/' + id)
+            .then(res => {
+                console.log(res.data)
+                setAuthors(res.data.Author.author)
             })
-            .catch((err) => {
+            .catch(err => console.log(err.response.data))
+    }, [id])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const newAuthor = {
+            author: authors
+        }
+
+        axios.patch('http://localhost:8000/api/authors/' + id, newAuthor)
+            .then(res => {
+                console.log(res.data);
+                setAuthors('');
+                navigate('/');
+            })
+            .catch(err => {
+                const errorResponse = err.response.data.errors;
+                const errorArr = Object.values(errorResponse).map(error => error.message);
+                setErrors(errorArr);
                 console.log(err);
             });
-    }, []);
+    };
 
     return (
-        <div>
-            {authors.map((author) => (
-                <fieldset key={author._id}>
-                    Update {author.author}
-                </fieldset>
-            ))}
+        <div>Update
+            <form onSubmit={handleSubmit}>
+                {errors.map((err, index) => <p key={index}>{err}</p>)}
+                <div>
+                    <label>Author:</label>
+                    <input
+                        type="text"
+                        value={authors}
+                        onChange={(e) => setAuthors(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Add</button>
+            </form>
         </div>
-    );
-};
+    )
+}
 
-export default Update;
+export default Update
